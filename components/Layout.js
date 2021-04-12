851156565b1cbf4b37c8bbef298086c920168548
Alia Peterson <70297733+alia-peterson@users.noreply.github.com>
2021-04-12 10:41:30 -0600

    Incorporate layout component

---
+++ components/Layout.js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.scss'
+
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>Alia Peterson Portfolio</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Philosopher&display=swap" rel="stylesheet" />
+      </Head>
+
+      <div className={styles.container}>
+        <Header />
+
+        {children}
+
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default Layout
--- pages/Contact.js
+++ pages/Contact.js
@@ -8,38 +8,32 @@ import Footer from '../components/Footer'
 
 export default function Contact() {
   return (
-    <div className={body.container}>
-      <Header />
+    <main className={body.main}>
+      <h2 className={body.title}>Contact</h2>
 
-      <main className={body.main}>
-        <h2 className={body.title}>Contact</h2>
+      <div className={styles.container}>
+        <a href='https://github.com/alia-peterson' target='_blank'>
+          <article className={styles.tarot}>
+            <Image src='/images/hat.png' height={200} width={200} />
+            <p>GitHub</p>
+          </article>
+        </a>
 
-        <div className={styles.container}>
-          <a href='https://github.com/alia-peterson' target='_blank'>
-            <article className={styles.tarot}>
-              <Image src='/images/hat.png' height={200} width={200} />
-              <p>GitHub</p>
-            </article>
-          </a>
+        <a href='https://www.linkedin.com/in/alia-peterson/' target='_blank'>
+          <article className={styles.tarot}>
+            <Image src='/images/plant bottle.png' height={280} width={200} />
+            <p>LinkedIn</p>
+          </article>
+        </a>
 
-          <a href='https://www.linkedin.com/in/alia-peterson/' target='_blank'>
-            <article className={styles.tarot}>
-              <Image src='/images/plant bottle.png' height={280} width={200} />
-              <p>LinkedIn</p>
-            </article>
-          </a>
+        <a href='mailto:alia.codes@pnwebs.com' target='_blank'>
+          <article className={styles.tarot}>
+            <Image src='/images/moon.png' height={200} width={200} />
+            <p>Email</p>
+          </article>
+        </a>
 
-          <a href='mailto:alia.codes@pnwebs.com' target='_blank'>
-            <article className={styles.tarot}>
-              <Image src='/images/moon.png' height={200} width={200} />
-              <p>Email</p>
-            </article>
-          </a>
-
-        </div>
-      </main>
-
-      <Footer />
-    </div>
+      </div>
+    </main>
   )
 }
