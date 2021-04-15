e907ecc18ffe6f59ff60647d2737857725e9e85a
Alia Peterson <70297733+alia-peterson@users.noreply.github.com>
2021-04-15 10:29:34 -0600

    Add context to handle theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import '../styles/globals.scss'
 import Layout from '../components/Layout'
+import { AppWrapper } from '../context/data-context'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AppWrapper>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AppWrapper>
   )
 }
 
