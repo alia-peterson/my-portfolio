cf81cc71dad985984512da707312234c20e22441
Alia Peterson <70297733+alia-peterson@users.noreply.github.com>
2021-04-26 00:01:38 -0600

    Update project information and styling

--- components/Card.js
+++ components/Card.js
@@ -12,22 +12,26 @@ export default function Card({ title, type, timeframe, description, technologies
       <h2>{title}</h2>
       <div className={styles.body}>
 
-        <div className={styles.text}>
-          <div>
+        <div>
+          <div className={styles.inner}>
             <div className={styles.link}>
               {deployed && <p><a href={deployed} target='_blank'>Live Website ☞</a></p>}
               {repository && <p><a href={repository} target='_blank'>Repository ☞</a></p>}
             </div>
-            <p><span>Team Type:</span> {type}</p>
-            <p><span>Timeframe:</span> {timeframe}</p>
-            <p><span>Technologies Used:</span> {techs}</p>
-            <p><span>Description:</span> {description}</p>
-          </div>
 
-          <Image src={image.src} height={image.height} width={image.width} />
+            <div>
+              <p><span>Team Type:</span> {type}</p>
+              <p><span>Timeframe:</span> {timeframe}</p>
+              <p><span>Technologies Used:</span> {techs}</p>
+            </div>
+          </div>
         </div>
 
+        <Image src={image.src} height={image.height} width={image.width} />
+
       </div>
+      
+      <p><span>Description:</span> {description}</p>
     </article>
   )
 }
--- data/projects.js
+++ data/projects.js
@@ -1,9 +1,23 @@
 const projects = [
   {
+    title: 'CourseChart',
+    type: 'Group',
+    timeframe: '2 Weeks',
+    description: 'A course planning tool to help instructors and subject matter experts plan activities for modules for a given course.',
+    technologies: ['React.js', 'Next.js', 'HTML', 'CSS', 'Javascript'],
+    deployed: '',
+    repository: 'https://github.com/CourseChart/course-chart-fe',
+    image: {
+      src: '/images/gems.png',
+      height: 100,
+      width: 180
+    }
+  },
+  {
     title: 'Personal Website',
     type: 'Solo',
     timeframe: 'Ongoing',
-    description: '',
+    description: 'My personal website! Showcases my latest projects and will likely be the place that I will test new technologies.',
     technologies: ['React.js', 'Next.js', 'HTML', 'SCSS', 'Javascript'],
     deployed: '',
     repository: '',
@@ -17,7 +31,7 @@ const projects = [
     title: 'Flashcards',
     type: 'Solo',
     timeframe: '1 Day',
-    description: '',
+    description: 'A flashcards application to help classmates and myself practice for technical interview questions.',
     technologies: ['React.js', 'HTML', 'CSS', 'Javascript'],
     deployed: 'https://interview-prep-turing.vercel.app/',
     repository: 'https://github.com/alia-peterson/interview-prep',
@@ -31,40 +45,26 @@ const projects = [
     title: 'Trivia Night',
     type: 'Solo',
     timeframe: '6 Days',
-    description: '',
+    description: 'An application that provides one location to find cocktail recipes and trivia questions to play with friends. Users can specify cocktail bases and trivia categories as well as save recipes for later use.',
     technologies: ['React.js', 'HTML', 'CSS', 'Javascript'],
     deployed: 'https://triviology.herokuapp.com/',
     repository: 'https://github.com/alia-peterson/trivia-night',
     image: {
-      src: '/images/potion bottle.png',
-      height: 200,
-      width: 100
+      src: '/images/candles.png',
+      height: 160,
+      width: 125
     }
   },
   {
     title: 'ActivAir',
     type: 'Group',
     timeframe: '9 Days',
-    description: '',
+    description: 'An application that reports weather and air quality for your current location and for any specified locations supported by the AirVisual API.',
     technologies: ['Vue.js', 'HTML', 'CSS', 'Javascript'],
     deployed: 'https://activ-air-alia-peterson.vercel.app/',
     repository: 'https://github.com/alia-peterson/ActivAir',
     image: {
       src: '/images/leaves.png',
-      height: 100,
-      width: 180
-    }
-  },
-  {
-    title: 'Rancid Tomatillos',
-    type: 'Pair',
-    timeframe: '2 Weeks',
-    description: '',
-    technologies: ['React.js', 'HTML', 'CSS', 'Javascript'],
-    deployed: 'https://rancid-tomatillos-khap.herokuapp.com/',
-    repository: 'https://github.com/alia-peterson/rancid-tomatillos',
-    image: {
-      src: '/images/gems.png',
       height: 100,
       width: 180
     }
