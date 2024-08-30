82c81951cad8eb6d36d3d2f4e2a504a65e9d591e
Alia Peterson <alia@pnwebs.com>
2024-08-29 20:07:39 -0600

    Add leaf image to home summary

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -8,16 +8,19 @@ export default function Home() {
 		<div className="w-full h-5/6 flex justify-center items-center">
 			<article
 				className={classNames(
-					'bg-white text-red-900 shadow-card border-2 border-red-900 rounded-xl w-large p-4',
+					'bg-white text-red-900 shadow-card border-2 border-red-900 rounded-xl w-large p-4 text-justify',
 					{ 'shadow-white': darkMode }
 				)}
 			>
-				Alia has found her passion in web development and loves to learn new
-				technologies and best practices while solving complex problems. Her
-				experience in designing and implementing scalable software architecture,
-				as well as managing and mentoring a team of developers, has been
-				extremely rewarding and she hopes to continue that trajectory for many
-				years to come.
+				<img className="float-right w-16" src="/images/leaves.png" />
+				<p>
+					Alia has found her passion in web development and loves to learn new
+					technologies and best practices while solving complex problems. Her
+					experience in designing and implementing scalable software
+					architecture, as well as managing and mentoring a team of developers,
+					has been extremely rewarding and she hopes to continue that trajectory
+					for many years to come.
+				</p>
 			</article>
 		</div>
 	);
